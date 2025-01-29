4757075122ff2bc9dc51ce7f181d510f7dfcb57c
kiqsmg <kiqsmg@gmail.com>
2025-01-29 06:00:48 -0300

    Add styling to the calculator buttons

--- Calculator-01/Calculator-app/App.js
+++ Calculator-01/Calculator-app/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Touchable, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const[ input, setInput ] = useState('')
@@ -17,7 +18,17 @@ export default function App() {
         keyboardType='numeric'
         />
         <View style={styles.buttonContainer}>
-          {['7','8','9','/','4','5','6','*','1','2','3','-','0','c','=','+']}
+          {['7','8','9','/','4','5','6','*','1','2','3','-','0','c','=','+'].map(
+            (item,index) =>(
+              <TouchableOpacity
+              key={index}
+              style={styles.button}
+              onPress={()=> onButtonPress(item)}
+              >
+                <Text style={styles.buttontext}>{item}</Text>
+              </TouchableOpacity>
+            )
+          )}
         </View>
       </View>
     </SafeAreaView>
@@ -26,15 +37,30 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex:1
   }, resultContainer: {
-
+    flex:2,
+    justifyContent: 'center',
+    alignItems:'flex-end'
   }, resultText: {
-
+    fontSize: 40
   }, inputContainer: {
-
+    flex:1,
+    justifyContent: 'center',
+    alignItems:'flex-end'
   }, inputText: {
-
+    fontSize: 30
   }, buttonContainer: {
-
+    flex: 7,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }, button: {
+    fontSize: 24,
+    width: "25%",
+    height: "20%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth:1,
+    borderColer:"#ccc"
   },
 });
