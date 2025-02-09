5b755143cd232a2e3a87528848917bb40598f451
kiqsmg <kiqsmg@gmail.com>
2025-02-09 05:31:45 -0300

    Create const for the calculator buttons

--- Calculator-01/Calculator-app/App.js
+++ Calculator-01/Calculator-app/App.js
@@ -2,6 +2,15 @@ import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 
 function App() {
+
+  const buttons = [
+    'C', '±', '%', '/',
+    '7', '8', '9', '*',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '.', '=',
+  ];
+  
   return (
     <div>App</div>
   )
