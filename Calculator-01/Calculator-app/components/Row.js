7016e573909e44be239c437cbf40554a500a109c
kiqsmg <kiqsmg@gmail.com>
2025-04-27 20:26:35 -0300

    clean row file

--- Calculator-01/Calculator-app/components/Row.js
+++ Calculator-01/Calculator-app/components/Row.js
@@ -1,12 +1,9 @@
-// Import StyleSheet and View Components
 import { StyleSheet, View } from "react-native";
 
-// Create Row reusable component that arranges its children horizontally using flexDirection
 const Row = ({ children }) => {
   return <View style={styles.container}>{children}</View>;
 };
 
-// create styles of Row
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
